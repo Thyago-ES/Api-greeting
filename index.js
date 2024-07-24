bfd58d0c56fb42d85bfd7f38622822aa241849dc
Thyago-ES <thyagorafael57@gmail.com>
2024-07-24 11:03:15 -0300

    fix: cors addiction

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.json([{ title: "Hello, world!", message: "Are you ok?" }]);
 });
 
+const port = process.env.PORT || 5000;
 app.listen(port);
